68151a00ea39881f0605679d71863df6d4121bbe
Shivam Goswami <shivamg1008@gmail.com>
2024-07-08 16:06:30 +0530

    enabled user registration and improved form using crispy

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_app.apps.DjangoAppConfig'
+    'django_app.apps.DjangoAppConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK='bootstrap5'
