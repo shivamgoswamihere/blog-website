a5a391c8f8346c49c8887ed43ffbf7c1267317ab
Shivam Goswami <shivamg1008@gmail.com>
2024-07-09 23:26:05 +0530

    added login ui

--- django_project/django_app/urls.py
+++ django_project/django_app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
